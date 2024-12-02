864949b3695c5aad4bb28931ed129fa2894b119d
nirduani1 <nirduani@icloud.com>
2024-12-02 16:59:34 +0200

    done

--- Primes.java
+++ Primes.java
@@ -1,5 +1,25 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int n = Integer.parseInt(args[0]) ;
+        boolean[] arr = new boolean[n + 1] ;
+        for (int i = 2; i <= n ; i++) {
+            arr[i] = true ;
+        }
+        for (int i = 2 ; i*i <= n ; i++) {
+            if (arr[i]){
+                for (int j = i*i ; j <= n ; j = j + i) {
+                    arr[j] = false ;
+                    
+                }
+
+            }
+            
+        }
+        System.out.println("Prime numbers up to " + n + ":") ;
+        for (int i = 2; i <= n ; i++) {
+            if (arr[i]){
+                System.out.println(i + " ");
+            }
+        }
     }
 }
